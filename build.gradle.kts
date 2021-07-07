8e320e87c90358a8257e5b5855ed47d6cf07c391
Neemias Almeida <neemias.junior@gmail.com>
2021-07-07 13:39:41 +0200

    revert commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,15 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.cloud:google-cloud-pubsub:1.113.5")
+    // does not work
+    implementation(platform("com.google.cloud:libraries-bom:20.7.0"))
+    implementation("com.google.cloud:google-cloud-pubsub")
+
+    // also does not work
+    // implementation("com.google.cloud:google-cloud-pubsub:1.113.5")
+
+    // works
+    // implementation("com.google.cloud:google-cloud-pubsub:1.101.0")
 }
 
 tasks {
@@ -26,7 +34,7 @@ tasks {
         manifest {
             attributes["Main-Class"] = "com.projects.kotlinplayground.ApplicationKt"
         }
-        from(configurations.runtime.get().map { if (it.isDirectory) it else zipTree(it) })
+        from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
         exclude("local.properties")
     }
     withType<KotlinCompile> {
